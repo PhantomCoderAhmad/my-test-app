41d2a73b332370e3b9b9f038c17dbfea446c0636
PhantomCoderAhmad <ahmadibrar0000@gmail.com>
2026-06-25 18:51:17 +0500

    deployment and yml files changes

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 set -e
 
+export HOME=/home/ahmad
+export GIT_SSH_COMMAND="ssh -i /home/ahmad/.ssh/id_ed25519 -o StrictHostKeyChecking=no"
+
 echo "🚀 Deploy shuru..."
 
 cd /var/www/html/myapp
 
-# Latest code lo
+# Main branch se latest code lo
 git pull origin main
 
-# Composer dependencies
-# composer install --no-dev --optimize-autoloader
-
-# NPM build (agar frontend hai)
-# npm install
-# npm run build
-
-# Laravel commands
 php artisan migrate --force
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-
 
 echo "✅ Deploy complete!"
